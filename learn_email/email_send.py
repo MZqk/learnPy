cd74591691528380e8f0cd9449549e580905eb1e
zqk <961504168@qq.com>
2019-07-20 16:13:18 +0800

    learn

---
+++ learn_email/email_send.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host="smtp.qq.com"  #设置服务器
+mail_user="1506603376@qq.com"    #用户名
+mail_pass="961504168"   #口令
+
+
+sender = '1506603376@qq.com'
+receivers = ['961504168@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+#创建一个带附件的实例
+message = MIMEMultipart()
+message['From'] = Header("W3Cschool教程", 'utf-8')
+message['To'] =  Header("测试", 'utf-8')
+subject = 'Python SMTP 邮件测试'
+message['Subject'] = Header(subject, 'utf-8')
+
+#邮件正文内容
+message.attach(MIMEText('这是W3Cschool教程Python 邮件发送测试……', 'plain', 'utf-8'))
+
+# 构造附件1，传送当前目录下的 test.txt 文件
+att1 = MIMEText(open('test.txt', 'rb').read(), 'base64', 'utf-8')
+att1["Content-Type"] = 'application/octet-stream'
+# 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+att1["Content-Disposition"] = 'attachment; filename="test.txt"'
+message.attach(att1)
+
+# 构造附件2，传送当前目录下的 w3cschool.txt 文件
+att2 = MIMEText(open('w3cschool.txt', 'rb').read(), 'base64', 'utf-8')
+att2["Content-Type"] = 'application/octet-stream'
+att2["Content-Disposition"] = 'attachment; filename="w3cschool.txt"'
+message.attach(att2)
+
+try:
+    smtpObj = smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print ("邮件发送成功")
+except smtplib.SMTPException:
+    print ("Error: 无法发送邮件")
---
+++ learn_re/re_oper.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import re
+
+print('example1:')
+print(re.match('www', 'www.w3cschool.cn').span())  # 在起始位置匹配
+print(re.match('cn', 'www.w3cschool.cn'))         # 不在起始位置匹配
+
+print('example2:')
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print ("matchObj.group() : ", matchObj.group())
+   print ("matchObj.group(1) : ", matchObj.group(1))
+   print ("matchObj.group(2) : ", matchObj.group(2))
+else:
+   print ("No match!!")
+print('example3:')
+line = "Cats are smarter than dogs";
+
+matchObj = re.match( r'dogs', line, re.M|re.I)
+if matchObj:
+   print ("match --> matchObj.group() : ", matchObj.group())
+else:
+   print ("No match!!")
+
+matchObj = re.search( r'dogs', line, re.M|re.I)
+if matchObj:
+   print ("search --> matchObj.group() : ", matchObj.group())
+else:
+   print ("No match!!")
+print('example4:')
+phone = "2004-959-559 # 这是一个电话号码"
+
+# 删除注释
+num = re.sub(r'#.*$', "", phone)
+print ("电话号码 : ", num)
+
+# 移除非数字的内容
+num = re.sub(r'\D', "", phone)
+print ("电话号码 : ", num)
